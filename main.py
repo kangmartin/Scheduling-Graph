e91925d590e28b76fc205e03d6f9a445be567a37
Martin Kang <martin.kang@efrei.net>
2024-04-12 23:44:02 +0200

    Centralisation sur un fichier main

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-def read_tasks_from_file(filename):
-    tasks = []
-    with open(filename, 'r') as file:
-        for line in file:
-            parts = line.strip().split()
-            if len(parts) > 1:
-                task_id = int(parts[0])
-                duration = int(parts[1])
-                predecessors = [int(x) for x in parts[2:]] if len(parts) > 2 else []
-                tasks.append((task_id, duration, predecessors))
-    return tasks
+from functions import *
 
+tasks = read_tasks_from_file("FichiersTest/test.txt")
+
+print(calcul_sommets(tasks))
+print(calcul_arc(tasks))
