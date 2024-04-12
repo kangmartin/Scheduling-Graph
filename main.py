46d2a7a066c502b454d3e50906f39ef9afbfd68e
Martin Kang <martin.kang@efrei.net>
2024-04-12 21:35:18 +0200

    Ajout de la fonction pour extraire et retourner une liste de tuples

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello 👋")
+def read_tasks_from_file(filename):
+    tasks = []
+    with open(filename, 'r') as file:
+        for line in file:
+            parts = line.strip().split()
+            if len(parts) > 1:
+                task_id = int(parts[0])
+                duration = int(parts[1])
+                predecessors = [int(x) for x in parts[2:]] if len(parts) > 2 else []
+                tasks.append((task_id, duration, predecessors))
+    return tasks
+
