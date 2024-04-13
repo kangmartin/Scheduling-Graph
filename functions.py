bd1f86ee4254fda705335f03099c16aec9d8f047
Martin Kang <martin.kang@efrei.net>
2024-04-13 10:10:10 +0200

    Ajout de la fonction d'affichage

--- functions.py
+++ functions.py
@@ -37,3 +37,25 @@ def calcul_arc(tasks):
     no_successors = all_tasks - has_predecessors
     arc_count += len(no_successors)
     return arc_count
+
+
+def afficher_graph(tasks):
+    print("Le fichier à:", calcul_sommets(tasks), "sommets")
+    print("Le fichier à:", calcul_arc(tasks), "arcs")
+    pred = []
+    for i in tasks:
+        pred.append(i[2])
+    for j in range(len(pred)):
+        if len(pred[j]) == 0:
+            print("0 ->", tasks[j][0], "= 0")
+        else:
+            for z in range(len(pred[j])):
+                print(pred[j][z], "->", tasks[j][0], "=", tasks[pred[j][z] - 1][1])
+
+    all_tasks = set(task[0] for task in tasks)
+    tasks_with_predecessors = set(pred for task in tasks for pred in task[2])
+    tasks_without_successors = all_tasks - tasks_with_predecessors
+    omega = max(all_tasks) + 1
+    for task in tasks:
+        if task[0] in tasks_without_successors:
+            print(task[0], "->", omega, "=", task[1])
--- main.py
+++ main.py
@@ -3,5 +3,4 @@ from functions import *
 tasks = lire_taches_fichier("FichiersTest/test.txt")
 
 print("Contenu des taches:", tasks)
-print("Le fichier à:", calcul_sommets(tasks), "sommets")
-print("Le fichier à:", calcul_arc(tasks), "arcs")
+afficher_graph(tasks)
