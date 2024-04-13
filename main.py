01fd0d7d288c2b67926afb85542429c7d84aec10
Martin Kang <martin.kang@efrei.net>
2024-04-13 12:39:27 +0200

    Mise a jour

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from functions import *
 tasks = lire_taches_fichier("FichiersTest/test.txt")
 
 print("Contenu des taches:", tasks)
-# afficher_graph(tasks)
+afficher_graph(tasks)
 creer_matrice(tasks)
