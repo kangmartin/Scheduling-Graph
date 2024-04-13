09e194774f7fbbd8b30b3eff8294b7c7f48f636b
Martin Kang <martin.kang@efrei.net>
2024-04-13 08:39:11 +0200

    Test des fonctions de lecture de fichiers / calculs de sommets et d'arcs

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from functions import *
 
-tasks = read_tasks_from_file("FichiersTest/test.txt")
+tasks = lire_taches_fichier("FichiersTest/test.txt")
 
-print(calcul_sommets(tasks))
-print(calcul_arc(tasks))
+print("Contenu des taches:", tasks)
+print("Le fichier à:", calcul_sommets(tasks), "sommets")
+print("Le fichier à:", calcul_arc(tasks), "arcs")
