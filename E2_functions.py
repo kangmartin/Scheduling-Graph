efae400ceaf343cc6d85895addf4b871e9816a1b
Martin Kang <martin.kang@efrei.net>
2024-04-21 20:42:52 +0200

    Ajout du graph d'ordonnancement

--- E2_functions.py
+++ E2_functions.py
@@ -55,6 +55,24 @@ def afficher_tableau_contraintes(tasks):
     # Afficher le tableau
     print("\nTableau de contraintes:")
     print(table)
+    print("\n* Création du graphe d’ordonnancement :\n")
+    pred = []
+    for i in tasks:
+        pred.append(i[2])
+    for j in range(len(pred)):
+        if len(pred[j]) == 0:
+            print("0 ->", tasks[j][0], "= 0")
+        else:
+            for z in range(len(pred[j])):
+                print(pred[j][z], "->", tasks[j][0], "=", tasks[pred[j][z] - 1][1])
+
+    all_tasks = set(task[0] for task in tasks)
+    tasks_with_predecessors = set(pred for task in tasks for pred in task[2])
+    tasks_without_successors = all_tasks - tasks_with_predecessors
+    omega = max(all_tasks) + 1
+    for task in tasks:
+        if task[0] in tasks_without_successors:
+            print(task[0], "->", omega, "=", task[1])
 
 
 def afficher_matrice(tasks):
